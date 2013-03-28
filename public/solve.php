4e84067d200392785e0a71bc867343352ce8ccb0
lmalysa <lukasz.malysa@gmail.com>
2013-03-28 23:55:47 +0100

    x

--- public/solve.php
+++ public/solve.php
@@ -2,5 +2,12 @@
 chdir(dirname(__DIR__));
 require_once('classes/Autoloader.php');
 spl_autoload_register(array('Autoloader','autoload'));
-$o = new Problem_1();
-echo $o->getResult();
+$strProblemClass = 'Problem_'.$_SERVER['argv'][1];
+try {
+    $o = new $strProblemClass();
+    echo $o->getResult();
+} catch (Autoloader_Exception $e) {
+    echo "No problem with given number";
+} catch (Exception $e) {
+    echo "There was an error: ".$e->getMessage()." - FILE: ".$e->getFile()." - LINE: ".$e->getLine();
+}
