f1c8f6ca96659c6792aab68f96bccf2991aca3f6
Lukasz Malysa <lukasz.malysa@medforum.pl>
2013-03-28 15:55:03 +0100

    my kommit

---
+++ classes/Logic.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Logic
+ *
+ * @author l.malysa
+ */
+abstract class Logic {
+    //put your code here
+}
---
+++ classes/Logic/Multiplies.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Multiplies
+ *
+ * @author l.malysa
+ */
+class Logic_Multiplies extends Logic {
+    
+    /**
+     * 
+     * 
+     * @param type $mixNumbers
+     * @return array
+     */
+    public function getMultipliesBy($mixNumbers, $intLimit)
+    {
+        $arrRet = array();
+        if (!is_array($mixNumbers)) {
+            $mixNumbers = array(0=>$mixNumbers);
+        }
+        for ($i=1;$i<$intLimit;$i++) {
+            if ($this->checkIfDivisible($i, $mixNumbers)) {
+                $arrRet[] = $i;
+            }
+        }
+        return $arrRet;
+    }
+    
+    /**
+     * 
+     * @param type $intOp
+     * @param type $arr
+     * @return boolean
+     */
+    public function checkIfDivisible($intOp, $arr) 
+    {
+        foreach ($arr as $int) {
+            if ($intOp % $intOp == 0) {
+               return true; 
+            }
+        }
+        return false;
+    }
+}
